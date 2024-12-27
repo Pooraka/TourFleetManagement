fbbd045b27a627238584a03151d4cd4bfdc9a2a4
K D P Hasendra <k.d.p.hasendra@gmail.com>
2024-12-27 23:36:01 +0530

    Login page edits

--- view/login.php
+++ view/login.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5">
     <title>Welcome</title>
+    <?php include_once "../includes/bootstrap_css_includes.php"?>
 </head>
-<body>
-    
+<body style="background-color:#f2f3ee">
+    <div class="container">
+        <div class="row" style="height:100px;background-color:red;">
+        </div>
+        <div class="row panel panel-default" style="height:500px;background-color:blue">
+            <div class="col-md-5 col-md-offset-1" 
+            style="height:100%;background-color:green;background-image:url('../images/img2.jpg');background-size:100%;
+            background-size: cover; background-repeat: no-repeat;border-radius:35px 0px 0px 35px">
+            </div>
+            <div class="col-md-5" style="height:100%;background-color:yellow" ></div>
+        </div>
+
+    </div>
 </body>
 </html>
