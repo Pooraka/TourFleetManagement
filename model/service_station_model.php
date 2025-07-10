68b549e21584a37c9e5a0f2c0fa801b8c84a7f2c
K D P Hasendra <k.d.p.hasendra@gmail.com>
2025-07-10 15:18:39 +0530

    Service Station Model - Prepared Statements

--- model/service_station_model.php
+++ model/service_station_model.php
@@ -9,22 +9,36 @@ class ServiceStation{
     public function addServiceStation($stationName,$address){
         
         $con = $GLOBALS["con"];
-
-        $sql = "INSERT INTO service_station(service_station_name,address) VALUES ('$stationName','$address')";
         
-        $con->query($sql) or die ($con->error);
+        $sql = "INSERT INTO service_station(service_station_name,address) VALUES (?,?)";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("ss", $stationName, $address); 
+
+        $stmt->execute();
+
         $serviceStationId=$con->insert_id;
+
+        $stmt->close();
+
         return $serviceStationId;
     }
     
     public function addServiceStationContact($serviceStationId,$number,$type){
         
         $con = $GLOBALS["con"];
-
-        $sql = "INSERT INTO service_station_contact(service_station_contact_number,contact_type,service_station_id) "
-                . "VALUES ('$number','$type','$serviceStationId')";
         
-        $con->query($sql) or die ($con->error);
+        $sql = "INSERT INTO service_station_contact(service_station_contact_number,contact_type,service_station_id) "
+            . "VALUES (?,?,?)";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("sii", $number, $type, $serviceStationId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function getServiceStations(){
@@ -40,29 +54,53 @@ class ServiceStation{
     public function getServiceStationContact($serviceStationId){
         
         $con = $GLOBALS["con"];
-
-        $sql = "SELECT * FROM service_station_contact WHERE service_station_id= '$serviceStationId'";
         
-        $result = $con->query($sql) or die ($con->error);
+        $sql = "SELECT * FROM service_station_contact WHERE service_station_id= ?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $serviceStationId); 
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $stmt->close();
+
         return $result;
     }
     
     public function removeServiceStation($serviceStationId){
         
         $con = $GLOBALS["con"];
-
-        $sql = "UPDATE service_station SET service_station_status='-1' WHERE service_station_id= '$serviceStationId'";
         
-        $con->query($sql) or die ($con->error);
+        $sql = "UPDATE service_station SET service_station_status='-1' WHERE service_station_id= ?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $serviceStationId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function getServiceStation($serviceStationId){
         
         $con = $GLOBALS["con"];
-
-        $sql = "SELECT * FROM service_station WHERE service_station_id ='$serviceStationId'";
         
-        $result = $con->query($sql) or die ($con->error);
+        $sql = "SELECT * FROM service_station WHERE service_station_id =?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $serviceStationId); 
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $stmt->close();
+
         return $result;
     }
     
@@ -70,17 +108,29 @@ class ServiceStation{
         
         $con = $GLOBALS["con"];
 
-        $sql = "UPDATE service_station SET service_station_name='$stationName', address='$address' WHERE service_station_id= '$serviceStationId'";
-        
-        $con->query($sql) or die ($con->error);
+        $sql = "UPDATE service_station SET service_station_name=?, address=? WHERE service_station_id= ?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("ssi", $stationName, $address, $serviceStationId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function removeServiceStationContact($serviceStationId){
         
         $con = $GLOBALS["con"];
 
-        $sql = "DELETE FROM service_station_contact WHERE service_station_id= '$serviceStationId'";
-        
-        $con->query($sql) or die ($con->error);
+        $sql = "DELETE FROM service_station_contact WHERE service_station_id= ?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $serviceStationId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
 }
