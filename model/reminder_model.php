0e1f30915247a5e6eb678d4ec392e38e1e9f2394
K D P Hasendra <k.d.p.hasendra@gmail.com>
2025-07-10 15:03:53 +0530

    Reminder model - Prepared Statements

--- model/reminder_model.php
+++ model/reminder_model.php
@@ -10,18 +10,33 @@ class Reminder{
         
         $con = $GLOBALS["con"];
         
-        $sql = "UPDATE reminder SET sent_date = '$sentDate' WHERE reminder_id = '$reminderId'";
-        
-        $con->query($sql) or die ($con->error);
+        $sql = "UPDATE reminder SET sent_date = ? WHERE reminder_id = ?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("si", $sentDate, $reminderId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function getReminder($reminderId){
         
         $con = $GLOBALS["con"];
         
-        $sql = "SELECT * FROM reminder WHERE reminder_id='$reminderId'";
-        
-        $result = $con->query($sql) or die ($con->error);
+        $sql = "SELECT * FROM reminder WHERE reminder_id=?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $reminderId); 
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $stmt->close();
+
         return $result;
     }
 }
