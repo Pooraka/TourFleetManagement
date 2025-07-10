871a8991aafe76394727d7f28f8e157d630f630d
K D P Hasendra <k.d.p.hasendra@gmail.com>
2025-07-10 15:12:40 +0530

    Service Detail Model - Prepared Statements

--- model/service_detail_model.php
+++ model/service_detail_model.php
@@ -11,10 +11,18 @@ class ServiceDetail{
         $con = $GLOBALS["con"];
         
         $sql ="INSERT INTO service_detail (bus_id,service_station_id,start_date,mileage_at_service,previous_bus_status,initiated_by) "
-                . "VALUES ('$busId','$serviceStationId','$startDate','$currentMileage','$previousBusStatus','$userId')";
-        
-        $con->query($sql) or die ($con->error);
+            . "VALUES (?,?,?,?,?,?)";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("iisiii", $busId, $serviceStationId, $startDate, $currentMileage, $previousBusStatus, $userId); 
+
+        $stmt->execute();
+
         $serviceId=$con->insert_id;
+
+        $stmt->close();
+
         return $serviceId;
     }
     
@@ -32,9 +40,15 @@ class ServiceDetail{
         
         $con = $GLOBALS["con"];
         
-        $sql = "UPDATE service_detail SET service_status = '-1', cancelled_by = '$userId', cancelled_date='$cancelledDate'  WHERE service_id ='$serviceId'";
-        
-        $con->query($sql) or die ($con->error);
+        $sql = "UPDATE service_detail SET service_status = '-1', cancelled_by = ?, cancelled_date=? WHERE service_id =?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("isi", $userId, $cancelledDate, $serviceId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function getServiceDetail($serviceId){
@@ -50,17 +64,25 @@ class ServiceDetail{
         $stmt->execute(); // Execute the statement
 
         $result = $stmt->get_result(); // Get the result object
+        
+        $stmt->close(); //Close the statement
         return $result;
     }
     
     public function completeService($serviceId,$completedDate,$cost,$invoice,$userId,$invoiceNumber){
         
         $con = $GLOBALS["con"];
-        
-        $sql = "UPDATE service_detail SET service_status ='2', completed_date='$completedDate', "
-                . "cost='$cost', invoice='$invoice', completed_by='$userId', invoice_number='$invoiceNumber' WHERE service_id ='$serviceId'";
-        
-        $con->query($sql) or die ($con->error);
+
+        $sql = "UPDATE service_detail SET service_status ='2', completed_date=?, "
+            . "cost=?, invoice=?, completed_by=?, invoice_number=? WHERE service_id =?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("sdsisi", $completedDate, $cost, $invoice, $userId, $invoiceNumber, $serviceId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function getPastServices(){
@@ -77,9 +99,15 @@ class ServiceDetail{
         
         $con = $GLOBALS["con"];
         
-        $sql = "UPDATE service_detail SET cost='$cost', invoice='$invoice', invoice_number='$invoiceNumber' WHERE service_id ='$serviceId'";
-        
-        $con->query($sql) or die ($con->error);
+        $sql = "UPDATE service_detail SET cost=?, invoice=?, invoice_number=? WHERE service_id =?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("dssi", $cost, $invoice, $invoiceNumber, $serviceId); 
+
+        $stmt->execute();
+
+        $stmt->close();
     }
     
     public function getPaymentPendingServiceStations(){
@@ -97,11 +125,20 @@ class ServiceDetail{
     public function getPaymentPendingServices($serviceStationId){
         
         $con = $GLOBALS["con"];
-        
+
         $sql = "SELECT * FROM service_detail d, bus b, service_station s WHERE "
-                . "d.service_station_id = s.service_station_id AND d.bus_id = b.bus_id AND d.service_station_id = '$serviceStationId' AND d.service_status='2'";
-        
-        $result = $con->query($sql) or die ($con->error);
+            . "d.service_station_id = s.service_station_id AND d.bus_id = b.bus_id AND d.service_station_id = ? AND d.service_status='2'";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $serviceStationId); 
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $stmt->close();
+
         return $result;
     }
     
@@ -109,9 +146,15 @@ class ServiceDetail{
         
         $con = $GLOBALS["con"];
         
-        $sql = "UPDATE service_detail SET payment_id='$paymentId', service_status='3' WHERE service_id ='$serviceId'";
-        
-        $con->query($sql) or die ($con->error);
+        $sql = "UPDATE service_detail SET payment_id=?, service_status='3' WHERE service_id =?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("ii", $paymentId, $serviceId); 
+
+        $stmt->execute();
+
+        $stmt->close();
         
     }
     
