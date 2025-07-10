43298196f976986129c61b9c2b3244c85dca34ff
K D P Hasendra <k.d.p.hasendra@gmail.com>
2025-07-10 14:53:06 +0530

    Module Model - Prepared Statements

--- model/module_model.php
+++ model/module_model.php
@@ -9,10 +9,20 @@ class Module{
     function getAllModules($role_id){
 
         $con = $GLOBALS["con"];
-
+        
         $sql = "SELECT m.module_id, m.module_name, m.module_icon, m.module_url, m.module_status "
-                . "FROM module m, role_module r WHERE m.module_id = r.module_id AND r.role_id= '$role_id'";
-        $result = $con->query($sql) or die ($con->error);
+            . "FROM module m, role_module r WHERE m.module_id = r.module_id AND r.role_id= ?";
+    
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param("i", $role_id);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $stmt->close();
+
         return $result;
     }
 }
