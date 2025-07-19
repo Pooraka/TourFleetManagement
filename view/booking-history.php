d632bb4804a33af13111d26b37d7a3b9cfe41679
K D P Hasendra <k.d.p.hasendra@gmail.com>
2025-07-19 09:03:55 +0530

    Booking History - Footer Now Display Page Totals

--- view/booking-history.php
+++ view/booking-history.php
@@ -180,12 +180,10 @@ $financeObj = new Finance();
                         </tbody>
                         <tfoot>
                             <tr>
-                                <th colspan="3" style="text-align:right">Total:</th>
-                                <th style="text-align: right;white-space: nowrap">
-                                    <?php 
-                                    echo "LKR " . number_format($totalAmount, 2); 
-                                    ?>
-                                </th>
+                                <th style="text-align:right;">Page Total:</th>
+                                <th style="text-align:right;white-space: nowrap"></th> 
+                                <th style="text-align:right;">Overall Total:</th>
+                                <th style="text-align:right;white-space: nowrap"></th> 
                                 <th></th>
                                 <th></th>
                             </tr>
@@ -204,26 +202,48 @@ $financeObj = new Finance();
 
         var dataTableOptions = {
             "pageLength": 5,
+            "order": [
+                [ 0, "desc" ] //Desc order by invoice date
+            ],
             // "scrollX": true,
             "footerCallback": function ( row, data, start, end, display ) {
                 var api = this.api();
-                var total = 0;
- 
-                // Get data from the 'Amount Paid' column (index 3)
-                var columnData = api.column(3, { search: 'applied' }).data();
                 
-                // Loop through the data
-                for (var i = 0; i < columnData.length; i++) {
-                    var amount = columnData[i];
+                // Calculate Total for the Current Page
+                var pageTotal = 0;
+                var pageData = api.column(3, { page: 'current' }).data(); // Get data for current page only
+                
+                for (var i = 0; i < pageData.length; i++) {
+                    var amount = pageData[i];
                     var numericValue = parseFloat(String(amount).replace(/LKR /g, '').replace(/,/g, ''));
                     if (!isNaN(numericValue)) {
-                       total += numericValue;
+                       pageTotal += numericValue;
                     }
                 }
- 
-                // Format total and update the specific footer cell
-                var formattedTotal = 'LKR ' + total.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                $(api.column(3).footer()).html(formattedTotal);
+                
+                // Format and display the page total in the 2nd footer cell
+                var formattedPageTotal = 'LKR ' + pageTotal.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                $(api.column(1).footer()).html(formattedPageTotal);
+                
+                
+                //Calculate Overall Total for All Filtered Pages
+                
+                var overallTotal = 0;
+                var overallData = api.column(3, { search: 'applied' }).data(); // Get data for all filtered pages
+                
+                for (var i = 0; i < overallData.length; i++) {
+                    
+                    var amount = overallData[i];
+                    var numericValue = parseFloat(String(amount).replace(/LKR /g, '').replace(/,/g, ''));
+                    
+                    if (!isNaN(numericValue)) {
+                       overallTotal += numericValue;
+                    }
+                }
+                
+                // Format and display the overall total in the 4th footer cell
+                var formattedOverallTotal = 'LKR ' + overallTotal.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                $(api.column(3).footer()).html(formattedOverallTotal);
             }
         };
         
